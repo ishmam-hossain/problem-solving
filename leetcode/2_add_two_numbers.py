168d29df31daadf8532b0e1a502f05a5a22dbd95
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-02-15 16:10:17 +0600

    at last :3

--- leetcode/2_add_two_numbers.py
+++ leetcode/2_add_two_numbers.py
@@ -1,6 +1,3 @@
-from math import log10
-
-
 class ListNode:
     def __init__(self, x, nxt=None):
         self.val = x
@@ -8,34 +5,41 @@ class ListNode:
 
 
 class Solution:
-    def get_number(self, ll):
-        nxt = ll
-        num_list = [nxt.val]
-        while nxt.next:
-            num_list.append(nxt.next.val)
-            nxt = nxt.next
-
-        res = 0
-        for i in range(len(num_list)):
-            res += num_list[i] * (10 ** (len(num_list) - i - 1))
-        return res
-
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        a = ListNode(0)
+        _carry = 0
+        res_node = ListNode(0)
+        cur_node = res_node
 
-        res = self.get_number(l1) + self.get_number(l2)
-        digits = []
-        while res:
-            rem = res % 10
-            digits.append(rem)
-            res = res // 10
+        while l1 is not None and l2 is not None:
+            _sum = l1.val + l2.val + _carry
+            _val = _sum % 10
+            _carry = _sum // 10
+            cur_node.next = ListNode(_val)
+            cur_node = cur_node.next
+            l1 = l1.next
+            l2 = l2.next
 
-        last_node = None
-        for digit in digits[::-1]:
-            a = ListNode(digit)
-            a.next = last_node
-            last_node = a
-        return a
+        if l1 is not None:
+            while l1 is not None:
+                _sum = l1.val + _carry
+                _val = _sum % 10
+                _carry = _sum // 10
+                cur_node.next = ListNode(_val)
+                cur_node = cur_node.next
+                l1 = l1.next
+        if l2 is not None:
+            while l2 is not None:
+                _sum = l2.val + _carry
+                _val = _sum % 10
+                _carry = _sum // 10
+                cur_node.next = ListNode(_val)
+                cur_node = cur_node.next
+                l2 = l2.next
+
+        if l1 is None and l2 is None and _carry:
+            cur_node.next = ListNode(_carry)
+
+        return res_node.next
 
 
 def PRINT(d):
@@ -46,49 +50,13 @@ def PRINT(d):
     print(d.val, end=' ')
 
 
-def get_digits(linked_list, out):
-    if linked_list is None:
-        return
-    out.append(linked_list.val)
-    get_digits(linked_list.next, out)
-    return out
+l1_nxt_1 = ListNode(9)
+ll1 = ListNode(9, l1_nxt_1)
 
+ll2 = ListNode(1)
 
-def reverse_linked_list(ll):
-    return ll[::-1]
+c = Solution()
+PRINT(c.addTwoNumbers(ll1, ll2))
 
-
-def new_add_test(ll1, ll2):
-    n1 = get_digits(ll1, [])
-    n2 = get_digits(ll2, [])
-
-    n = max(len(n1), len(n2))
-    _carry = 0
-    res = []
-    new_node = None
-    prev_node = None
-    for i in range(n):
-        local_sum = n1[i] + n2[i] + _carry
-        _carry = local_sum // 10
-        res.append(local_sum % 10)
-        new_node = ListNode(local_sum % 10)
-        new_node.next = prev_node
-        prev_node = new_node
-
-    return new_node
-
-
-l1_nxt_2 = ListNode(3)
-l1_nxt_1 = ListNode(4, l1_nxt_2)
-l1 = ListNode(2, l1_nxt_1)
-
-l2_nxt_2 = ListNode(4)
-l2_nxt_1 = ListNode(6, l2_nxt_2)
-l2 = ListNode(5, l2_nxt_1)
-
-# c = Solution()
-# data = c.addTwoNumbers(l1, l2)
-# print(PRINT(data))
-PRINT(new_add_test(l1, l2))
 
 
