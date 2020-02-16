43cb8e24cb2303df0ec0f8d231d7e6f0f0b07f09
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-02-16 14:25:55 +0600

    111

--- leetcode/111_minimum_depth_of_bin_tree.py
+++ leetcode/111_minimum_depth_of_bin_tree.py
@@ -3,18 +3,10 @@ from collections import deque
 
 
 class TreeNode:
-    def __init__(self, x, left=None, right=None):
+    def __init__(self, x):
         self.val = x
-        self.left = left
-        self.right = right
-
-
-def min_depth_recursive(root: TreeNode) -> int:
-    def get_min_depth(head, depth=0):
-        if head is None:
-            return depth
-        return get_min_depth(head.left, depth + 1) and get_min_depth(head.right, depth + 1)
-    return get_min_depth(root)
+        self.left = None
+        self.right = None
 
 
 class Solution:
@@ -24,32 +16,15 @@ class Solution:
 
         q = deque()
         depth = 1
-        q.append(root)
+        q.append({'node': root, 'depth': depth})
 
         while q:
             cur = q.popleft()
-            if cur.left is None and cur.right is None:
+            depth = cur['depth']
+            node = cur['node']
+            if node.left is None and node.right is None:
                 return depth
-            depth += 1
-            if cur.left is not None:
-                q.append(cur.left)
-            if cur.right is not None:
-                q.append(cur.right)
-
-
-left_9 = TreeNode(9)
-right_7 = TreeNode(7)
-right_15 = TreeNode(15)
-right_20 = TreeNode(20, right_15, right_7)
-r = TreeNode(3, left_9, right_20)
-
-r1 = TreeNode(1)
-r1.right = TreeNode(2)
-
-r2 = TreeNode(1)
-
-s = Solution()
-print(s.minDepth(r))
-print(s.minDepth(r1))
-print(s.minDepth(r2))
-
+            if node.left is not None:
+                q.append({'node': node.left, 'depth': depth+1})
+            if node.right is not None:
+                q.append({'node': node.right, 'depth': depth+1})
