b8c2092c7ff6cd420d7ce6c5cb1224e2012f6c9b
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-02-17 16:12:47 +0600

    136 :D

--- leetcode/136_single_integer.py
+++ leetcode/136_single_integer.py
@@ -9,6 +9,36 @@ class Solution:
 
         return visited[0]
 
+    def single_num_dict(self, nums):
+        count = {}
+        for n in nums:
+            temp = count.get(n)
+            if temp:
+                count.pop(n)
+            else:
+                count[n] = 1
+        return count.__iter__().__next__()
+
+    def single_num_set(self, nums):
+        unq_nums = set()
+        for n in nums:
+            if n in unq_nums:
+                unq_nums.remove(n)
+            else:
+                unq_nums.add(n)
+
+        return unq_nums.__iter__().__next__()
+
+    def single_num_xor(self, nums):
+        res = 0
+        for n in nums:
+            res = res ^ n
+
+        return res
+
 
 s = Solution()
-print(s.singleNumber([2, 2, 4, 4, 1]))
+# print(s.singleNumber([2, 2, 4, 4, 1]))
+# print(s.single_num_dict([2, 2, 4, 4, 1]))
+# print(s.single_num_set([2, 2, 4, 4, 1]))
+print(s.single_num_xor([2, 2, 4, 4, 1]))
