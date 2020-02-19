0d6457f60658cd8f0db1cebeabae3c3defcaee64
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-02-19 17:19:29 +0600

    one edge case left :3

---
+++ leetcode/8_string_to_integer.py
@@ -0,0 +1,50 @@
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        my_num = 0
+        pos = 0
+        is_neg = False
+        is_pos = False
+        entered = False
+        got_out = False
+        INT_MIN = -2**31
+        INT_MAX = 2 ** 31 - 1
+
+        for c in str[::-1]:
+            ascii_val = ord(c)
+            if 48 <= ascii_val <= 57:
+                my_num += ord(c) % 48 * 10 ** pos
+                pos += 1
+                entered = True
+            elif entered:
+                got_out = True
+
+            if c == '.':
+                my_num = 0
+                entered = False
+                got_out = False
+                pos = 0
+
+            if c == '-' and entered and got_out:
+                is_neg = True
+
+            if c == '+' and entered and got_out:
+                is_pos = True
+
+            if entered and got_out and c not in ('-', '+', ' '):
+                return 0
+
+            if is_neg and is_pos:
+                return 0
+
+        my_num = my_num if not is_neg else my_num * -1
+
+        if my_num < INT_MIN:
+            return INT_MIN
+        elif my_num > INT_MAX:
+            return INT_MAX
+
+        return my_num
+
+
+s = Solution()
+print(s.myAtoi("7.83389339.9883902"))
