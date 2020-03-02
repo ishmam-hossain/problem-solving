9d68a6d0f971a9ad2a8713a7fb1591b7a5a9c58e
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-03-02 22:34:33 +0600

    112

---
+++ leetcode/112_path_to_leaf_sum.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if root is None:
+            return False
+
+        if root.val == sum and root.left is None and root.right is None:
+            return True
+
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, root.val)
+
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+
+s = Solution()
+print(s.hasPathSum(stringToTreeNode("[5,4,8,11,null,13,4,7,2,null,null,null,1]"), 22))
