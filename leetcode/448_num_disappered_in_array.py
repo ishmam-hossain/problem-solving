678a85e09ee6e7bcb4ba8d349185027bdb17316c
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-03-08 13:04:06 +0600

    448

---
+++ leetcode/448_num_disappered_in_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        if not nums:
+            return []
+        max_num = max(max(nums), len(nums))
+        res = [None] * max_num
+
+        for n in nums:
+            res[n-1] = n
+
+        return [i+1 for i, n in enumerate(res) if n is None]
+
+
+s = Solution()
+print(s.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
