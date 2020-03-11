638e42c9341b39a0b17ef4d03cd8a65aaabd2654
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-03-11 17:18:26 +0600

    905 optimized

--- leetcode/905_sort_by_parity.py
+++ leetcode/905_sort_by_parity.py
@@ -1,5 +1,5 @@
 class Solution:
-    def sortArrayByParity(self, A: List[int]) -> List[int]:
+    def sortArrayByParity(self, A):
         evens = []
         odds = []
         for n in A:
@@ -9,3 +9,23 @@ class Solution:
                 odds.append(n)
         return evens + odds
 
+    def in_place(self, A):
+        lo = 0
+        hi = len(A) - 1
+
+        while lo < hi:
+            if A[lo] % 2 != 0 and A[hi] % 2 == 0:
+                A[lo], A[hi] = A[hi], A[lo]
+                lo += 1
+                hi -= 1
+
+            if A[lo] % 2 == 0:
+                lo += 1
+            if A[hi] % 2 != 0:
+                hi -= 1
+
+        return A
+
+
+s = Solution()
+print(s.in_place([0, 1]))
