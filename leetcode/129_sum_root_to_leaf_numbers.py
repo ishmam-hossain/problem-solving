d09e04138974f66f20bf9f8b351156f4dddd5632
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-03-20 17:06:04 +0600

    129 with str

--- leetcode/129_sum_root_to_leaf_numbers.py
+++ leetcode/129_sum_root_to_leaf_numbers.py
@@ -17,6 +17,16 @@ class Solution:
 
         return get_sum(root, 0)
 
+    def with_str(self, root):
+        def get_sum(head, cur):
+            if head is None:
+                return 0
+            if head.left is None and head.right is None:
+                return int(cur + str(head.val))
+            return get_sum(head.left, cur + str(head.val)) + get_sum(head.right, cur + str(head.val))
+
+        return get_sum(root, "")
+
 
 t = TreeNode(4)
 t.left = TreeNode(9)
@@ -24,4 +34,4 @@ t.right = TreeNode(0)
 t.left.right = TreeNode(1)
 
 s = Solution()
-print(s.sumNumbers(None))
+print(s.with_str(t))
