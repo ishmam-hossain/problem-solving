b922ad11758227b69e8f406e21e5e5aae3c90f0d
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-03-24 16:38:55 +0600

    102

---
+++ leetcode/102_bin_tree_level_order_traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        vals = {}
+
+        def level(head, depth):
+            if head is None:
+                return
+
+            vals.setdefault(depth, []).append(head.val)
+
+            level(head.left, depth + 1)
+            level(head.right, depth + 1)
+
+        level(root, 1)
+
+        return vals.values()
