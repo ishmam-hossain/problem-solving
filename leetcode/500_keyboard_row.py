197e2a3856d47406f236897dceb428c1e6b05b90
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-03-28 18:47:31 +0600

    500

--- leetcode/500_keyboard_row.py
+++ leetcode/500_keyboard_row.py
@@ -19,6 +19,21 @@ class Solution:
 
         return final
 
+    def concised_version(self, words):
+        row_to_keys = {
+            1: {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"},
+            2: {"A", "S", "D", "F", "G", "H", "J", "K", "L"},
+            3: {"Z", "X", "C", "V", "B", "N", "M"}
+        }
+
+        final = []
+        for word in words:
+            res = {k for k, v in row_to_keys.items() for c in word if c.upper() in v}
+            if len(res) == 1:
+                final.append(word)
+
+        return final
+
 
 s = Solution()
 print(s.findWords(["Hello", "Alaska", "Dad", "Peace"]))
