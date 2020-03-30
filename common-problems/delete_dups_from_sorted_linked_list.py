70ebe9cdba1af39e83d792e3a43e79d69ccd2ba7
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-03-30 22:23:33 +0600

    delete dups

---
+++ common-problems/delete_dups_from_sorted_linked_list.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the removeDuplicates function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def removeDuplicates(head):
+    cur = head
+
+    while cur.next:
+        if cur.data == cur.next.data:
+            cur.next = cur.next.next
+        else:
+            cur = cur.next
+    return head
+if __name__ == '__main__':
