9e5360072df7db80206c213ccfe9c45710f4c4e0
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-03-30 18:40:58 +0600

    863

--- leetcode/863_all_nodes_distance_K_in_bin_tree.py
+++ leetcode/863_all_nodes_distance_K_in_bin_tree.py
@@ -8,25 +8,36 @@ class TreeNode:
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, K: int):
-        res = {}
-        self.target_level = -1
+        class Solution:
+            def distanceK(self, root: TreeNode, target: TreeNode, K: int) -> List[int]:
+                res = {}
+                self.target_level = -1
 
-        def get_nodes(head, depth, is_left):
-            if head is None:
-                return
+                def get_nodes(head, depth, is_left):
+                    if head is None:
+                        return
 
-            res.setdefault(depth, []).append(head.val)
-            if head.val == target:
-                self.target_level = depth
+                    res.setdefault(depth, []).append(head.val)
+                    if head.val == target:
+                        self.target_level = depth
 
-            get_nodes(head.left, depth + (-1 if is_left else 1), is_left)
-            get_nodes(head.right, depth + (-1 if is_left else 1), is_left)
+                    get_nodes(head.left, depth + (-1 if is_left else 1), is_left)
+                    get_nodes(head.right, depth + (-1 if is_left else 1), is_left)
 
-        get_nodes(root.left, -1, is_left=True)
-        get_nodes(root.right, 1, is_left=False)
+                get_nodes(root.left, -1, is_left=True)
+                get_nodes(root.right, 1, is_left=False)
 
-        final = [val for key, temp in res.items() if abs(key-self.target_level) == K for val in temp]
-        return final
+                final = []
+                for key, val in res.items():
+                    if key < 0 and self.target_level < 0:
+                        diff = abs(abs(key) - abs(self.target_level))
+                    else:
+                        diff = abs(key - self.target_level)
+                    if diff == K:
+                        final.extend(val)
+                return final
+
+        # final = [val for key, temp in res.items() if abs(key-self.target_level) == K for val in temp]
 
 
 tree = TreeNode(3)
@@ -40,4 +51,4 @@ tree.right.left = TreeNode(0)
 tree.right.right = TreeNode(8)
 
 s = Solution()
-print(s.distanceK(tree, 7, 4))
+print(s.distanceK(tree, 5, 2))
