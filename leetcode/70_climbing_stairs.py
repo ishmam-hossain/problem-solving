7b31cda147e7a14ce9757673ba9d4738cd7203b0
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-04-03 01:07:22 +0600

    70

--- leetcode/70_climbing_stairs.py
+++ leetcode/70_climbing_stairs.py
@@ -2,6 +2,22 @@ from functools import lru_cache
 
 
 class Solution:
+    def best_solution(self, n):
+        f = s = 1
+        for i in range(2, n + 1, 1):
+            f, s = s, f + s
+
+        return s
+
+    def second_best_solution(self, n):
+        dp = [-1] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1, 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[-1]
+
     @lru_cache(maxsize=128)
     def climbStairs(self, n: int) -> int:
         if n == 0:
