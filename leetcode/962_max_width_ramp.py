9cfb531a303caa47bf67b9b37267452c6ee5f7f3
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-04-02 20:59:37 +0600

    962 -done

--- leetcode/962_max_width_ramp.py
+++ leetcode/962_max_width_ramp.py
@@ -1,10 +1,27 @@
 class Solution:
-    def maxWidthRamp(self, A: List[int]) -> int:
-        """O(N^2) solution. TLE on the way :3"""
+    def maxWidthRamp(self, A) -> int:
+        seen = {}
+        for i, n in enumerate(A):
+            seen.setdefault(n, []).append(i)
+
+        ind = float("inf")
         ramp = 0
-        for i, m in enumerate(A):
-            for j, n in enumerate(A[i+1:], i+1):
-                if A[i] <= A[j]:
-                    ramp = max(ramp, j-i)
+        for n in sorted(A):
+            ramp = max(ramp, seen[n][-1] - ind)
+            ind = min(ind, seen[n][0])
 
         return ramp
+
+    def bad_solution(self, A):
+        ramp = 0
+        for i, m in enumerate(A):
+            for j, n in enumerate(A[i + 1:], i + 1):
+                if A[i] <= A[j]:
+                    ramp = max(ramp, j - i)
+
+        return ramp
+
+
+s = Solution()
+# print(s.maxWidthRamp([6, 0, 8, 2, 1, 5]))
+print(s.maxWidthRamp([9, 8, 1, 0, 1, 9, 4, 0, 4, 1]))
