f10f652eb1018fe33b928947a42a59a6dc41e825
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-04-03 18:59:37 +0600

    543

--- leetcode/543_dia_of_bin_tree.py
+++ leetcode/543_dia_of_bin_tree.py
@@ -8,38 +8,14 @@ class TreeNode:
 
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        if not root:
-            return 0
-        left = self.get_depth(root, 0, False)
-        right = self.get_depth(root, 0, True)
-        return left + right + (1 if left > 0 and right > 0 else 0)
-
-    def get_depth(self, head, depth, min_depth=True):
-        if head is None:
-            return depth - 1
-
-        if not min_depth:
-            return min(self.get_depth(head.left, depth + 1), self.get_depth(head.right, depth + 1))
-        else:
-            return max(self.get_depth(head.left, depth + 1), self.get_depth(head.right, depth + 1))
+        self.final = 0
+        def max_depth(head):
+            if head is None:
+                return 0
+            left, right = max_depth(head.left), max_depth(head.right)
+            self.final = max(self.final, left+right)
+            return max(left, right) + 1
+        max_depth(root)
+        return self.final
 
 
-t = TreeNode(2)
-t.left = TreeNode(3)
-t.left.left = TreeNode(1)
-# t.left.left = TreeNode(4)
-# t.left.right = TreeNode(5)
-
-s = Solution()
-print(s.diameterOfBinaryTree(t))
-
-"""
-          1
-         / \
-        2   3
-       / \     
-      4   5 
-
-[2,3,null,1]
-[4,-7,-3,null,null,-9,-3,9,-7,-4,null,6,null,-6,-6,null,null,0,6,5,null,9,null,null,-1,-4,null,null,null,-2]
-"""
