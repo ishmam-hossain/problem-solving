dc7c9a6d4c185d4fe8f8c3e9cc3a1aa4b573d0fc
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-04-04 14:08:14 +0600

    283 -optimized

--- leetcode/283_move_zeroes.py
+++ leetcode/283_move_zeroes.py
@@ -1,4 +1,27 @@
 class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        """This is the most optimized one"""
+        zero_at = num_at = 0
+
+        while zero_at < len(nums) and num_at < len(nums):
+            if nums[zero_at] != 0:
+                zero_at += 1
+            if nums[num_at] == 0:
+                num_at += 1
+            if num_at < zero_at:
+                num_at += 1
+                continue
+
+            if zero_at < len(nums) and num_at < len(nums) and nums[zero_at] == 0 and nums[num_at] != 0:
+                nums[zero_at], nums[num_at] = nums[num_at], nums[zero_at]
+                zero_at += 1
+                num_at += 1
+
+
+class SolutionBad:
     def moveZeroes(self, nums) -> None:
         """
         Do not return anything, modify nums in-place instead.
@@ -8,17 +31,14 @@ class Solution:
                 if n == 0:
                     nums[j], nums[j+1] = nums[j+1], nums[j]
 
+    def move_zeroes_not_efficient(self, nums) -> None:
+        count = 0
+        for i, n in enumerate(nums):
+            if n == 0:
+                nums.remove(nums[i])
+                nums.append(0)
+                count += 1
 
-def move_zeroes_efficient(nums) -> None:
-    count = 0
-    for i, n in enumerate(nums):
-        if n == 0:
-            nums.remove(nums[i])
-            nums.append(0)
-            count += 1
-
-
-print(move_zeroes_efficient([0, 0, 1]))
 
 """
 Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
