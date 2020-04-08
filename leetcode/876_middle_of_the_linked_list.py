cf6d7352eb8ecc21b147d1dcf105ef342dd13821
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-04-08 13:47:05 +0600

    876

--- leetcode/876_middle_of_the_linked_list.py
+++ leetcode/876_middle_of_the_linked_list.py
@@ -6,6 +6,23 @@ class ListNode:
 
 
 class Solution:
+    def better_solution(self, head):
+        tot = 0
+        cur = head
+        while cur:
+            tot += 1
+            cur = cur.next
+
+        mid = (tot // 2) + 1
+
+        cur = head
+        pos = 1
+        while cur:
+            if pos == mid:
+                return cur
+            cur = cur.next
+            pos += 1
+
     def middleNode(self, head: ListNode) -> ListNode:
         tot = 0
         cur = head
