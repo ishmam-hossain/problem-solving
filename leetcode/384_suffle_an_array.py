3d66ae0359da7f9091857732ef1215c74e33dfb3
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-04-12 15:56:46 +0600

    384

--- leetcode/384_suffle_an_array.py
+++ leetcode/384_suffle_an_array.py
@@ -5,29 +5,22 @@ class Solution:
 
     def __init__(self, nums):
         self.actual = nums
+        self.shuffled = random.sample(self.actual, len(self.actual))
 
     def reset(self):
         """
         Resets the array to its original configuration and return it.
         """
-        return self.actual
+        self.shuffled = self.actual[:]
+        return self.shuffled
 
     def shuffle(self):
         """
         Returns a random shuffling of the array.
         """
-        length = len(self.actual)
-        res = self.actual[:]
-        if length > 1:
-            for i in range(randint(1, length)):
-                res[i], res[length-1-i] = res[length-1-i], res[i]
-
-        return res
-
+        return random.sample(self.shuffled, len(self.shuffled))
 
 # Your Solution object will be instantiated and called as such:
-nums = [1, 2, 3, 4, 5, 6]
-obj = Solution(nums)
-param_1 = obj.reset()
-param_2 = obj.shuffle()
-print(param_2)
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
