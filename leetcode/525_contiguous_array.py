2e9cc89d0fc0b7577f27cb75ad5595b31a841d8f
ishmam-hossain <ishmam.hossain101111@gmail.com>
2020-04-14 14:15:20 +0600

    525

---
+++ leetcode/525_contiguous_array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMaxLength(self, nums) -> int:
+        count = 0
+        max_length = 0
+        table = {0: 0}
+        for index, num in enumerate(nums, 1):
+            if num == 0:
+                count -= 1
+            else:
+                count += 1
+
+            if count in table:
+                max_length = max(max_length, index - table[count])
+            else:
+                table[count] = index
+
+        return max_length
+
+s = Solution()
+print(s.findMaxLength([0, 1, 1, 1, 0, 0]))
