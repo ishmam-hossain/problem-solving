0382445d60377a443ea43b2b8a2c482eb83fb435
ishmam-hossain <ishmam.hossain101111@gmail.com>
2022-10-22 20:46:50 +0600

    optimized 2011

--- leetcode/2011_final_val_of_var_after_op.py
+++ leetcode/2011_final_val_of_var_after_op.py
@@ -7,3 +7,23 @@ class Solution:
             "X--": -1
         }
         return sum([ops.get(op) for op in operations])
+
+    def finalValueAfterOperationsOptimized(self, operations: List[str]) -> int:
+        ops = {
+            "++X": 1,
+            "X++": 1,
+            "--X": -1,
+            "X--": -1
+        }
+        res = 0
+        for op in operations:
+            res += ops.get(op)
+        return res
+
+    def finalValueAfterOperationsMoreOptimized(self, operations: List[str]) -> int:
+        res = 0
+        for op in operations:
+            res += 1 if (op[0] == "+" or op[-1] == "+") else -1
+        return res
+
+
