3ccbe02f6c45ec67b6fb1ade25e225ff77a79bf1
ishmam-hossain <ishmam.hossain101111@gmail.com>
2022-10-22 20:37:15 +0600

    1480 constant space

--- leetcode/1480_running_sum.py
+++ leetcode/1480_running_sum.py
@@ -7,3 +7,15 @@ class Solution:
             res[i] = running_sum
 
         return res
+
+    def runningSumBetter(self, nums: List[int]) -> List[int]:
+        running_sum = 0
+        for i, n in enumerate(nums):
+            running_sum += n
+            nums[i] = running_sum
+
+        return nums
+
+
+
+
