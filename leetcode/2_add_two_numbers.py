2635fae23c5e390b713c9eb29dc0e8f282203e07
ishmam-hossain <ishmam.hossain101111@gmail.com>
2022-10-23 16:50:35 +0600

    2 better solution

--- leetcode/2_add_two_numbers.py
+++ leetcode/2_add_two_numbers.py
@@ -41,6 +41,31 @@ class Solution:
 
         return res_node.next
 
+    def new_solution(self, l1, l2):
+        carry = 0
+        head = ListNode(0)
+        cur = head
+
+        while l1 or l2:
+            if l1 and l2:
+                node_sum = l1.val + l2.val + carry
+                l1 = l1.next
+                l2 = l2.next
+            elif not l2:
+                node_sum = l1.val + carry
+                l1 = l1.next
+            elif not l1:
+                node_sum = l2.val + carry
+                l2 = l2.next
+
+            carry = node_sum // 10
+            val = node_sum % 10
+            cur.next = ListNode(val)
+            cur = cur.next
+
+        cur.next = ListNode(carry) if carry else None
+        return head.next
+
 
 def PRINT(d):
     if d is None:
@@ -57,6 +82,7 @@ ll2 = ListNode(1)
 
 c = Solution()
 PRINT(c.addTwoNumbers(ll1, ll2))
+PRINT(c.new_solution(ll1, ll2))
 
 
 
