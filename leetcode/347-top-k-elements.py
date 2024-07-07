d634e4501424a3bd532a3888962fd8b4c1da068f
Ishmam Hossain <37960094+ishmam-hossain@users.noreply.github.com>
2024-07-07 14:32:26 +0200

    Create 347-top-k-elements.py

---
+++ leetcode/347-top-k-elements.py
@@ -0,0 +1,33 @@
+from typing import List
+from collections import Counter
+
+
+def top_k_frequent(nums: List[int], k: int) -> List[int]:
+    counter = Counter(nums)
+    frequency_list = []
+    for x in range(len(nums) + 1):
+        frequency_list.append([])
+
+    for num, count in counter.items():
+        frequency_list[count].append(num)
+
+    res = []
+
+    for item in reversed(frequency_list):
+        if k == 0:
+            return res
+        if len(item) > k:
+            item = item[:k]
+            k = 0
+            res.extend(item)
+        else:
+            res.extend(item)
+            k -= len(item)
+
+    return res
+
+
+a = [1, 1, 2, 2, 2, 4, 5, 5, 5]
+ka = 2
+
+print(top_k_frequent(a, ka))
